b2ed4a6916d4dcf6793dbf8cd40d59d7c37afa64
Giordano Paolo <p.giordano.1320@gmail.com>
2021-12-07 08:54:32 +0100

    added HTML of es 2 in Express

--- Express/ES02_CrootServer/static/index.js
+++ Express/ES02_CrootServer/static/index.js
@@ -1,30 +1,23 @@
+"use strict"
+
 $(document).ready(function() {
 
-    $("#btnInvia").on("click", function() {
-        let request = inviaRichiesta("get", "/api/servizio1", {"nome":"hhhhhhhhhh"});
-        request.fail(errore);
-        request.done(function(data) {
-            if(data.length > 0) alert(JSON.stringify(data));
-            else alert("Nessuna corrispondenza trovata")
+    let divIntestazione = $("#divIntestazione")
+    let divCollections = $("#divCollections")
+    let table = $("#mainTable")
+    let divDettagli = $("#divDettagli")
+    let currentCollection = "";
 
-        });
-    });
-    $("#btnInvia2").on("click", function() {
-        let request = inviaRichiesta("patch", "/api/servizio2", {"nome":"Unico", "vampires":3});
-        request.fail(errore);
-        request.done(function(data) {
-            if(data.modifiedCount > 0){
-                alert("Aggiornamento eseguito correttamente");
-            }
-            else alert("Nessuna corrispondenza trovata")
-        });
-    });
-    
-    $("#btnInvia3").on("click", function() {
-        let request = inviaRichiesta("get", "/api/servizio3/m/brown");
-        request.fail(errore);
-        request.done(function(data) {
-            console.log(data);
-        });
-    });
+    let request = inviaRichiesta("get", "api/getCollections");
+    request.fail(errore)
+    request.done(function(collections) {
+      
+	  
+    })
+
+
+   
+   
+   
+   
 });
